cb449dbfbac0ebe9190a324426c49064a8215cd4
maxwell <maxwellbenton@gmail.com>
2020-12-27 13:47:37 -0500

    functioning speech to text with randomized question

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React, {useState, useEffect, useRef} from 'react'
 import './App.css';
+import Speech from 'react-speech';
+
+const topicTypes = [
+  "animals",
+  "places"
+]
+
+const defaultData = {
+  topics: topicTypes.reduce((ac,a) => ({...ac,[a]:''}),{}),
+  topicsLoading: topicTypes.length
+}
 
 function App() {
+  const [data, setData] = useState(defaultData)
+  const [inputText, setInputText] = useState("")
+  const speechComponent = useRef(null);
+  
+  useEffect(() => {
+    topicTypes.forEach((type) => {
+      fetch(`https://raw.githubusercontent.com/maxwellbenton/alexa-talker-site/master/data/${type}.json`)
+      .then(res => res.json())
+      .then(retrievedData => {
+        setData(oldData => ({
+          ...oldData,
+          topics: {
+            ...oldData.topics,
+            [`${type}`]: retrievedData
+          },
+          topicsLoading: oldData.topicsLoading - 1
+        }))
+      })
+    })
+
+    setInputText("Generating Alexa command...")
+  }, [])
+
+  useEffect(() => {
+    console.log(data)
+    if (data.topicsLoading <= 0) {
+      
+      let generalTopics = Object.keys(data.topics)
+      let generalTopic = generalTopics[Math.floor(Math.random() * generalTopics.length)]
+      let topic = data.topics[generalTopic][Math.floor(Math.random() * data.topics[generalTopic].length)]
+      let randomCommand = `Alexa, tell me about ${topic}`
+      setInputText(randomCommand)
+    }
+  }, [data])
+
+  function handleChange(input) {
+    setInputText(input.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={inputText} onChange={handleChange}/>
+      <Speech 
+        ref={speechComponent} 
+        text={inputText} 
+        voice="Google UK English Male"
+      />
     </div>
   );
 }
